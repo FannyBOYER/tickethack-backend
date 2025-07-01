60b28dd89dae002f40b1dd798ae284a2aba94e84
Dal ferro Christian <christiandalferro@gmail.com>
2025-07-01 16:12:54 +0200

    routes de reservation

--- models/trips.js
+++ models/trips.js
@@ -10,3 +10,5 @@ const tripSchema = mongoose.Schema({
 const Trip = mongoose.model('trips', tripSchema);
 
 module.exports = Trip; 
+
+
--- routes/reservations.js
+++ routes/reservations.js
@@ -3,14 +3,46 @@ const router = express.Router();
 const Reservation = require('../models/reservations')
 
 
-router.get('/',(req,res)=>{
-    Reservation.find()
+router.get('/:id',(req,res)=>{
+    const {id} = req.params;
+    const newReservation = new Reservation({
+        date: new Date(),
+        purchase: false,
+        idTrip: id
+    })
+    console.log(id)
+
+    newReservation.save()
                 .then(data => {
                     console.log(data);
-                    res.json({result: true})
+                    res.json({result: true , add: data})
                 })
 })
 
 
+router.delete('/:id',(req,res)=>{
+    const {id} = req.params;
+    console.log(id)
+    
+    Reservation.deleteOne({_id: id})
+                .then(data=>{
+                    console.log(data);
+                    res.json({result: true , delete: data});
+                })
+});
+
+router.get('/purchase/:id',(req,res)=>{
+    const {id} = req.params;
+    console.log(id)
+    
+    
+
+
+    Reservation.updateMany({_id: id})
+                .then(data=>{
+                    console.log(data);
+                    res.json({result: true , update: data});
+                })
+});
 
 module.exports = router;
