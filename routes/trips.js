5dfcf0612ce58ffd17742d2062128f404fde8a54
Fanny <fannyboyer1d@gmail.com>
2025-07-02 16:40:44 +0200

    finalback

--- routes/trips.js
+++ routes/trips.js
@@ -14,15 +14,28 @@ router.post('/', (req, res) => {
     let endDate = new Date(`${date}T23:59:59Z`)
 
     // Pour chaque objet du tablo, comparer date avec celle du body, si celle du body comrpise dans date de chaque élément, alors afficher l'élement 
+    // On gère aussi la casse 
+
+    function escapeRegex(text){ //Gestion caractères spéciaux et espaces
+        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+    }
+
+    // Fonction pour nettoyer la chaîne : suppression espaces + normalisation regex
+    function normalize(text) {
+      return escapeRegex(text.replace(/\s+/g, ''));
+    }
+
+    const departureRegex = new RegExp(`^${normalize(departure)}$`, 'i');
+    const arrivalRegex = new RegExp(`^${normalize(arrival)}$`, 'i');
 
     Trip.find({
-        departure : departure,
-        arrival : arrival, 
+        departure: { $regex: new RegExp(departure.trim().split('').join('\\s*'), 'i') },
+         arrival: { $regex: new RegExp(arrival.trim().split('').join('\\s*'), 'i') }, 
         date : {
             $gte: startDate,
-            $lt: endDate
+            $lt: endDate,
         }
-    })                
+    })               
         .then(data => {
         res.json({trajet : data, result : true})
 
